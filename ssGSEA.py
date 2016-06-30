6436c8cb188a3e528af0c459fe42f2ddae92f4d7
luoa <luoa@uw.edu>
2016-06-30 10:26:21 -0700

    added ssgsea methods for sorting on simulated phenotypes

--- ssGSEA.py
+++ ssGSEA.py
@@ -3,9 +3,12 @@ This file contains script for emulating the ssGSEA algorithm from Barbie et al. 
 '''
 
 import pickle
-import matplotlib.pyplot as plotter
 from data_models import gene_set
+import gaussian_sampling
+import model_fit
+from simulation import *
 
+import numpy as np
 '''
 Calculates the enrichment score of this profile
 
@@ -16,6 +19,7 @@ samples = a mapping of gene names -> intensity values
 omega = the weight bro of the P_G^W term. 0 = just like that russian stat test
 
 returns an array representing the intermediate ES values for each step along the random walk
+To find the total enrichment score, take the sum of all values in this array
 '''
 def calculate_enrichment_score(gene_set, expressions, omega):
     #list of keys in order to parse to get low -> high values
@@ -30,38 +34,117 @@ def calculate_enrichment_score(gene_set, expressions, omega):
     P_NG_denominator = len(expressions) - len(gene_set)
     P_NG = lambda : P_NG_numerator / P_NG_denominator
 
-    #print("P_GW\tP_NG")
     scores = []
-    ES = 0
-    #i = 0
     for gene in keys_sorted:
         #hitting a gene in the gene set
         if gene in gene_set:
             P_GW_numerator += abs(expressions[gene]) ** omega
         else:
             P_NG_numerator += 1
-        #i += 1
 
-        #print(str(P_GW()) + "\t\t" + str(P_NG()))
-
-        ES += P_GW() - P_NG()
         scores.append(P_GW() - P_NG())
 
     return scores
 
-if __name__ == "__main__":
-    gene_set = pickle.load(open("BC_gene_sets.pkl", 'rb'))
+#Below methods are meant to download and dump scores and shit man
+
+'''
+ToDo: migrate and cleanup, dumps pickles with models based on the enrichments scores of each data set
+'''
+def get_model_pathways():
+    gene_sets = pickle.load(open("BC_good_gene_sets.pkl", 'rb'))
     samples = pickle.load(open("BC_sample_profiles.pkl", 'rb'))
-    print("loaded!")
+    print("Data loaded!")
 
-    profile = samples[4].profiles
-    gene_set = [gene for gene in gene_set[2].genes if gene in profile.keys()]
-    print(gene_set)
+    models = {}
+    #for each gene set
+    for set in gene_sets:
+        gene_set = set.genes
 
-    expressions = {}
-    for gene in profile.keys():
-        expressions[gene] = profile[gene].intensity
+        #go through all the samples and calculate the ES
+        scores = []
+        for id in samples.keys():
+            profile = samples[id].profiles
+            expressions = {}
 
-    scores = calculate_enrichment_score(gene_set, expressions, 2)
-    plotter.plot([i + 1 for i in range(0, len(scores))], scores)
-    plotter.savefig("SSGSEA.png")
+            for gene in profile.keys():
+                expressions[gene] = profile[gene].intensity
+
+            score = calculate_enrichment_score(gene_set, expressions, 0.25)
+            scores.append(sum(score))
+
+        #add the models bro!
+        model = model_fit.fit_test_model([[x] for x in scores])
+        models[set.set_name] = model
+        print("Set " + str(set.set_name) + " done!")
+
+    pickle.dump(models, open("BC_enriched_set_models.pkl", 'wb'))
+
+def get_model_scores():
+    gene_sets = pickle.load(open("BC_good_gene_sets.pkl", 'rb'))
+    samples = pickle.load(open("BC_sample_profiles.pkl", 'rb'))
+    print("Data loaded!")
+
+    paths = {}
+    #for each gene set
+    for set in gene_sets:
+        gene_set = set.genes
+
+        #go through all the samples and calculate the ES
+        scores = {}
+        for id in samples.keys():
+            profile = samples[id].profiles
+            expressions = {}
+
+            for gene in profile.keys():
+                expressions[gene] = profile[gene].intensity
+
+            score = calculate_enrichment_score(gene_set, expressions, 0.25)
+            scores[id] = sum(score)
+
+        paths[set.set_name] = scores
+        print("1 set done enriched scores!")
+
+    pickle.dump(paths, open("BC_ssgsea_scores.pkl", 'wb'))
+
+
+'''
+ToDo: make extensible and reusable
+'''
+def ssgsea_on_simulation(gene, models, profiles):
+
+    #simulate phenotype labels and seperate the class0's/class1's
+    model = models[gene]
+    phenotypes = simulate_data(model, gene, profiles, len(profiles))
+
+    #these contain the expressions for those in class0/class1 respectively
+    class0 = []
+    class1 = []
+    for profile in profiles:
+        expression = profile.profiles[gene].intensity
+        key = profile.sample_num
+        if phenotypes[key] == 1:
+            class1.append(expression)
+        else:
+            class0.append(expression)
+
+    #we then want the expression profiles of the ERBB2 gene to work correctly
+    #creating models!
+    sigma0 = np.std(class0)
+    mu0 = np.mean(class0)
+
+    sigma1 = np.std(class1)
+    mu1 = np.mean(class1)
+
+    gaussian_sampling.plot_multidist([100000,100000], [mu0, mu1], [sigma0, sigma1], [1, 1], str(gene) + "_Sim_Phenotypes")
+
+if __name__ == "__main__":
+    #first load all the models you want
+    models = pickle.load(open("BC_trained_models.pkl", 'rb'))
+    profiles = pickle.load(open("BC_expression_profiles.pkl", 'rb'))
+
+    print("Loaded models!")
+
+    ssgsea_on_simulation("ERBB2", models, profiles)
+    gaussian_sampling.plot_gauss_mix_model(models["PLXNB3"], "PLXNB3")
+
