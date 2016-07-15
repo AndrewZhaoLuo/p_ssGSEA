e7c0dca3fc0b45158169de1f12adb2ab5a2e24d9
luoa <luoa@uw.edu>
2016-07-15 11:20:59 -0700

    reformatted print statements

--- analyze_enrichment.py
+++ analyze_enrichment.py
@@ -72,7 +72,7 @@ def rank_by_t_test_keyed(enrichment_scores, phenotypes, master_gene):
     '''
     As above, but returns a map mapping master_gene to the results
     '''
-    print("\t\tExamining gene " + master_gene)
+    print("\t\tRunning T-Test " + master_gene)
     return {master_gene: rank_by_t_test(enrichment_scores, phenotypes)}
 
 def evaluate_rankings(rankings, gene_sets, master_gene):
