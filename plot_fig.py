55c97fead112d0d969eb719ceffd446acc41d382
luoa <luoa@uw.edu>
2016-07-19 13:14:19 -0700

    plotting of effectiveness for individual master genes

--- plot_fig.py
+++ plot_fig.py
@@ -5,6 +5,8 @@ This file contains methods for plotting various graphs given various data points
 import os
 import pickle
 import cache_codec
+import numpy as np
+from matplotlib import pyplot as plotter
 
 enrichment_ranks = pickle.load(open(os.getcwd() + '/Data/AppCache/BC/CachedEnrichmentPValueSplit.pkl', 'rb'))
 
@@ -30,3 +32,43 @@ def get_ranking_data(enrichment_ranks):
             ranks.append(trial_rank)
 
     return ranks
+
+def make_graph_against_sets_picked(gene, ranks, x_range):
+    '''
+    currently averages all sets
+    '''
+
+    pop = 0
+    y_best = []
+    y_worst = []
+    y_median = []
+    y_mean = []
+    for x in x_range:
+        y_local = []
+        for trial in ranks:
+            pop = sum(trial)
+            tot_sets = sum(trial)
+            picked_sets = sum(trial[:(x + 1)])
+            percent_picked = picked_sets / tot_sets
+            y_local.append(percent_picked)
+
+        y_mean.append(sum(y_local) / len(y_local))
+        y_median.append(np.median(y_local))
+        y_best.append(max(y_local))
+        y_worst.append(min(y_local))
+
+    x_axis = [x for x in x_range]
+    plotter.plot(x_axis, y_mean, label='mean')
+    plotter.plot(x_axis, y_median, label='median')
+    plotter.plot(x_axis, y_best, label='best')
+    plotter.plot(x_axis, y_worst, label='worse')
+    plotter.legend(loc='upper right')
+
+    plotter.savefig("./Pictures/Analysis_" + gene + " popularity: " + str(pop))
+    plotter.close()
+
+def make_graph_against_sets_all(ranks, x_range):
+    print()
+
+ranking = get_ranking_data(enrichment_ranks)
+make_graph_against_sets_picked("ERBB2", ranking, range(0, len(ranking[0])))
